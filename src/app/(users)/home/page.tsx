88c58987ae0b3af71ece872365e69b0062ff9b38
Icyss999 <chungtaktest@gmail.com>
2026-06-27 22:56:39 +0700

    fix bugs

--- src/app/(users)/home/page.tsx
+++ src/app/(users)/home/page.tsx
@@ -13,7 +13,9 @@ export default async function Page() {
   })
   const data = await res.json()
   const session = await auth.api.getSession({
-    headers: await headers()
+    headers: {
+      Cookie: cookieStore.toString()
+    }
   })
 
 
